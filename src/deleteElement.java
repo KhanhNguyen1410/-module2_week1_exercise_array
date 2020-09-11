0a3c1aa98e6c32e6a295f36e260addf8f5ab13dd
Khanh-Nobi-JR <69251285+Khanh-Nobi-JR@users.noreply.github.com>
2020-09-11 13:05:00 +0700

    add

---
+++ src/deleteElement.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class deleteElement {
+    public static void main(String[] args) {
+        int[] array = {10,9,8,7,6,5};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your number");
+        int number = scanner.nextInt();
+        int index = del(array,number);
+        for (int i = index; i <array.length-1 ; i++) {
+            array[i] = array[i + 1];
+        } array[array.length - 1] = 0;
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf( array[i]+ ", ");
+        }
+    }
+    public static int del(int[]arr,int number){
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (number == arr[i]){
+                index = i;
+            }
+        }
+        return index;
+    }
+}
